e3310d55d18c415fb804482184a6ed6aacf8eb4e
githubuser997 <69780152+githubuser997@users.noreply.github.com>
2020-08-17 21:26:42 +0200

    Add files via upload

---
+++ py.py
@@ -0,0 +1,8 @@
+#Hi Guys, Let's Start Making A Simple Project By Using Numbers
+
+#First, Let's Make A Code That Counts To Ten:
+
+#But, You Should Know That Every Code We Write Every Code We Know
+
+for i in range (0,11):
+    print (i)
